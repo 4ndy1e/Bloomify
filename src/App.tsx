dc678916f0dc9690a0dfba39878cf3b3efdefecc
Andy Le <4ndyle@gmail.com>
2025-03-13 18:44:42 -0700

    add video on home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import homeVideo from "./assets/homeVideo.mp4";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Demo from "./components/demo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <video loop autoPlay muted>
+          <source src={homeVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <Demo />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
